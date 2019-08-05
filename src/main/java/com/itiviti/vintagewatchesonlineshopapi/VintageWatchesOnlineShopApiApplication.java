ef57fa2b1889028264ee7caadded6e5f91324581
SorinCrainic <sorin.crainic.86@gmail.com>
2019-08-06 01:23:30 +0300

    2019.08.06-Initial commit

---
+++ src/main/java/com/itiviti/vintagewatchesonlineshopapi/VintageWatchesOnlineShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.itiviti.vintagewatchesonlineshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintageWatchesOnlineShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintageWatchesOnlineShopApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/itiviti/vintagewatchesonlineshopapi/domain/Product.java
@@ -0,0 +1,66 @@
+package com.itiviti.vintagewatchesonlineshopapi.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    public String name;
+    private int quantity;
+    private double price;
+    private String imagePath;
+    private String productDescription;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+}
