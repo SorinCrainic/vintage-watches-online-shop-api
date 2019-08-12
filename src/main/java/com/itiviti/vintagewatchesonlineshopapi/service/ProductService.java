548ce1d5300929e90f797de542119ed1e9fcb3cd
SorinCrainic <sorin.crainic.86@gmail.com>
2019-08-13 00:04:19 +0300

    2019.08.12-Add new layers to project

--- src/main/java/com/itiviti/vintagewatchesonlineshopapi/service/ProductService.java
+++ src/main/java/com/itiviti/vintagewatchesonlineshopapi/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.itiviti.vintagewatchesonlineshopapi.service;
 
+import com.itiviti.vintagewatchesonlineshopapi.domain.Product;
 import com.itiviti.vintagewatchesonlineshopapi.repository.ProductRepository;
+import com.itiviti.vintagewatchesonlineshopapi.transfer.CreateProductRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,22 @@ public class ProductService {
     @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    //Method for creating a product (CRUD)
+    public Product createProduct(CreateProductRequest request) {
+
+
+        Product product = new Product();
+
+        product.setName(request.getName());
+        product.setQuantity(request.getQuantity());
+        product.setPrice(request.getPrice());
+        product.setImagePath(request.getImagePath());
+        product.setProductDescription(request.getProductDescription());
+        product.setProductRate(request.getProductRate());
+
+        return productRepository.save(product);
+
     }
 }
