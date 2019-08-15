8d85541b3921a8d8c7ba9538dd366c5a453f6471
SorinCrainic <sorin.crainic.86@gmail.com>
2019-08-15 23:42:53 +0300

    2019.08.15-Add updateProduct and deleteProduct methods

--- src/main/java/com/itiviti/vintagewatchesonlineshopapi/service/ProductService.java
+++ src/main/java/com/itiviti/vintagewatchesonlineshopapi/service/ProductService.java
@@ -1,13 +1,15 @@
 package com.itiviti.vintagewatchesonlineshopapi.service;
 
-import com.itiviti.vintagewatchesonlineshopapi.domain.Product;
-import com.itiviti.vintagewatchesonlineshopapi.exceptions.ProductNotFoundException;
-import com.itiviti.vintagewatchesonlineshopapi.repository.ProductRepository;
-import com.itiviti.vintagewatchesonlineshopapi.transfer.CreateProductRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+        import com.itiviti.vintagewatchesonlineshopapi.domain.Product;
+        import com.itiviti.vintagewatchesonlineshopapi.exceptions.ProductNotFoundException;
+        import com.itiviti.vintagewatchesonlineshopapi.repository.ProductRepository;
+        import com.itiviti.vintagewatchesonlineshopapi.transfer.CreateProductRequest;
+        import com.itiviti.vintagewatchesonlineshopapi.transfer.UpdateProductRequest;
+        import org.slf4j.Logger;
+        import org.slf4j.LoggerFactory;
+        import org.springframework.beans.BeanUtils;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
@@ -40,15 +42,27 @@ public class ProductService {
         return productRepository.save(product);
     }
 
-    //Method for retrieving a product from db by productId
+    //Method for RETRIEVING a product from db by productId (cRud)
     public Product getProduct(long id) throws ProductNotFoundException {
         LOGGER.info("Retrieving product {}", id);
         //using Optional wit orElseThrow; using Lambda expressions (anonymous methods)
         return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException("Product " + id + " not exist."));
     }
 
-    //Method for UPDATE-ing a product (cRud)
-//    public Product updateProduct(){
-//
-//    }
+    //Method for UPDATE-ing a product (crUd)
+    public Product updateProduct(long id, UpdateProductRequest requestForUpdate) throws ProductNotFoundException {
+        LOGGER.info("Update-ing product {} with {}", id, requestForUpdate);
+        Product productToBeUpdated = getProduct(id);
+
+        BeanUtils.copyProperties(requestForUpdate, productToBeUpdated);
+
+        return productRepository.save(productToBeUpdated);
+    }
+
+    //Method for DELETE-ing a product (cruD)
+    public void deleteProduct(long id) {
+        LOGGER.info("Delete-ing product {}", id);
+        productRepository.deleteById(id);
+        LOGGER.info("Deleted product {}", id);
+    }
 }
---
+++ src/main/java/com/itiviti/vintagewatchesonlineshopapi/transfer/UpdateProductRequest.java
@@ -0,0 +1,82 @@
+package com.itiviti.vintagewatchesonlineshopapi.transfer;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+public class UpdateProductRequest {
+
+    @NotNull
+    public String name;
+    @Min(0)
+    private Integer quantity;
+    @NotNull
+    private double price;
+    private String imagePath;
+    @NotNull
+    private String productDescription;
+    @NotNull
+    @Min(0)
+    @Max(10)
+    private double productRate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public double getProductRate() {
+        return productRate;
+    }
+
+    public void setProductRate(double productRate) {
+        this.productRate = productRate;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateProductRequest{" +
+                "name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", imagePath='" + imagePath + '\'' +
+                ", productDescription='" + productDescription + '\'' +
+                ", productRate=" + productRate +
+                '}';
+    }
+}
