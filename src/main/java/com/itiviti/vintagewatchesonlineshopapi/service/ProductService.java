22333687b0a872cb26c0959f1c15254265baa3da
SorinCrainic <sorin.crainic.86@gmail.com>
2019-08-15 22:36:55 +0300

    2019.08.15-Add positive integration tests (for getProduct method)

--- src/main/java/com/itiviti/vintagewatchesonlineshopapi/service/ProductService.java
+++ src/main/java/com/itiviti/vintagewatchesonlineshopapi/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.itiviti.vintagewatchesonlineshopapi.service;
 
 import com.itiviti.vintagewatchesonlineshopapi.domain.Product;
+import com.itiviti.vintagewatchesonlineshopapi.exceptions.ProductNotFoundException;
 import com.itiviti.vintagewatchesonlineshopapi.repository.ProductRepository;
 import com.itiviti.vintagewatchesonlineshopapi.transfer.CreateProductRequest;
 import org.slf4j.Logger;
@@ -22,7 +23,7 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    //Method for creating a product (CRUD)
+    //Method for CREATING a product (Crud)
     public Product createProduct(CreateProductRequest request) {
 
         LOGGER.info("Creating product {}", request);
@@ -38,4 +39,16 @@ public class ProductService {
 
         return productRepository.save(product);
     }
+
+    //Method for retrieving a product from db by productId
+    public Product getProduct(long id) throws ProductNotFoundException {
+        LOGGER.info("Retrieving product {}", id);
+        //using Optional wit orElseThrow; using Lambda expressions (anonymous methods)
+        return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException("Product " + id + " not exist."));
+    }
+
+    //Method for UPDATE-ing a product (cRud)
+//    public Product updateProduct(){
+//
+//    }
 }
--- src/test/java/com/itiviti/vintagewatchesonlineshopapi/ProductServiceIntegrationTests.java
+++ src/test/java/com/itiviti/vintagewatchesonlineshopapi/ProductServiceIntegrationTests.java
@@ -1,6 +1,7 @@
 package com.itiviti.vintagewatchesonlineshopapi;
 
 import com.itiviti.vintagewatchesonlineshopapi.domain.Product;
+import com.itiviti.vintagewatchesonlineshopapi.exceptions.ProductNotFoundException;
 import com.itiviti.vintagewatchesonlineshopapi.service.ProductService;
 import com.itiviti.vintagewatchesonlineshopapi.transfer.CreateProductRequest;
 import org.junit.Test;
@@ -26,10 +27,14 @@ public class ProductServiceIntegrationTests {
     @Autowired
     private ProductService productServiceTest;
 
-    //Test for method createProduct: positive test (valid request)
+    //1.1 Test for method createProduct: positive test (valid request)
     @Test
     public void testCreateProduct_whenValidRequest_thenReturnCreatedProduct() {
 
+        createProductTest();
+    }
+
+    private Product createProductTest() {
         CreateProductRequest requestPositiveTest = new CreateProductRequest();
         requestPositiveTest.setName("Ruhla");
         requestPositiveTest.setPrice(399.00);
@@ -44,15 +49,26 @@ public class ProductServiceIntegrationTests {
         assertThat(createdProductTest.getName(), is(requestPositiveTest.getName()));
         assertThat(createdProductTest.getPrice(), is(requestPositiveTest.getPrice()));
         assertThat(createdProductTest.getQuantity(), is(requestPositiveTest.getQuantity()));
-        assertThat(createdProductTest.getProductRate(),is(requestPositiveTest.getProductRate()));
-        assertThat(createdProductTest.getProductDescription(),is(requestPositiveTest.getProductDescription()));
+        assertThat(createdProductTest.getProductRate(), is(requestPositiveTest.getProductRate()));
+        assertThat(createdProductTest.getProductDescription(), is(requestPositiveTest.getProductDescription()));
+
+        return createdProductTest;
     }
 
-    //Test for method createProduct: negative test (not valid request -> missing mandatory parameter, for example)
-    @Test (expected = TransactionSystemException.class)
+    //1.2 Test for method createProduct: negative test (not valid request -> missing mandatory parameter, for example)
+    @Test(expected = TransactionSystemException.class)
     public void testCreateProduct_whenNotValidRequest_thenThrowException() {
 
         CreateProductRequest requestNegativeTest = new CreateProductRequest();
         productServiceTest.createProduct(requestNegativeTest);
+    }
+
+    //2.1 Test for method getProduct: positive test (valid request)
+    @Test
+    public void testGetProduct_whenValidRequest_thenReturnRetrievedProduct() throws ProductNotFoundException {
+        Product createdProductTest = createProductTest();
+        Product retrievedProduct = productServiceTest.getProduct(createdProductTest.getId());
+        assertThat(retrievedProduct, notNullValue());
+        assertThat(retrievedProduct.getId(), is(createdProductTest.getId()));
     }
 }
