90fb4da857ac5913ce68f0dec8b45222ce2a5c92
SorinCrainic <sorin.crainic.86@gmail.com>
2019-08-24 16:46:26 +0300

    2019.08.24-Update ShoppingCartController

--- src/main/java/com/itiviti/vintagewatchesonlineshopapi/web/ShoppingCartController.java
+++ src/main/java/com/itiviti/vintagewatchesonlineshopapi/web/ShoppingCartController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/shopping_cart")
 public class ShoppingCartController {
